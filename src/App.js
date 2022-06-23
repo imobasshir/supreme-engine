3caafdbabf96d1c2bc56c289bfdd6c49e7f245d2
Mobasshir Imam <iibrahimimam24@gmail.com>
2022-06-23 13:39:12 +0530

    done

--- src/App.js
+++ src/App.js
@@ -2,12 +2,38 @@ import './App.css';
 import { Header } from "./Components/Header";
 import { Main } from "./Components/Main";
 import { Footer } from "./Components/Footer";
+import { useState } from 'react';
 
 function App() {
+  const onDelete = (todo) => {
+    console.log(todo);
+    setTodos(todos.filter((e) => {
+      return e !== todo;
+    }));
+  }
+  const [todos, setTodos] = useState(
+    [
+      {
+        sno: 1,
+        title: "Take foods",
+        desc: "You need to go to market",
+      },
+      {
+        sno: 2,
+        title: "Learn React",
+        desc: "You need to go to college",
+      },
+      {
+        sno: 3,
+        title: "Watch Movies",
+        desc: "You need to go to mall",
+      },
+    ]
+  )
   return (
     <>
-      <Header title = "My To Do App"/>
-      <Main />
+      <Header title="My To Do App" />
+      <Main todos={todos} onDelete={onDelete} />
       <Footer />
     </>
   );
--- src/Components/Main.js
+++ src/Components/Main.js
@@ -1,8 +1,19 @@
 import React from 'react'
 
-export const Main = () => {
+export const Main = (props) => {
     return (
-        <div>main</div>
+        <div className="container">
+            <h4 className="text-center">To Do List</h4>
+            {props.todos.map((todo) => {
+                return (
+                    <>
+                        <h6 className='my-3'>{todo.title}</h6>
+                        <p className='my-3'>{todo.desc}</p>
+                        <button className="btn btn-sm btn-danger" key={props.sno} onClick={props.onDelete}>Delete</button>
+                    </>
+                )
+            })}
+        </div>
     )
 }
 
